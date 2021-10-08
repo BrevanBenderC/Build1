01836716ed2a096ffe7683d3257e5901dcddd11c
Brevan Bender <bcaybender@gmail.com>
2021-10-08 13:23:36 -0700

    Button and paragraph added cypress test added

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-// import functions and grab DOM elements
+const btn = document.getElementById('generate');
+const randomNum = document.getElementById('random-num');
 
-// initialize global state
 
-// set event listeners 
-// get user input
-// use user input to update state 
-// update DOM to reflect the new state
-
+btn.addEventListener('click', ()=> {
+    let number = Math.random();
+    randomNum.innerHTML = `${number}`;
+})
